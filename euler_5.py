e93c4b641216aa9e933f0b3fb27a433924f4297a
Somiari Lucky <somiarilucky@gmail.com>
2017-09-06 01:12:42 +0100

    Update solution
    Removed redundant code and changed incrementing value to 20.

--- euler_5.py
+++ euler_5.py
@@ -5,16 +5,13 @@ def checknum(number):
     return try_num % 3 == 0 and try_num % 4 == 0 and try_num % 7 == 0 and try_num % 8 == 0 and try_num % 9 == 0 \
            and try_num % 11 == 0 and try_num % 13 == 0 and try_num % 16 == 0 and try_num % 17 == 0 and try_num % 19 == 0 and try_num % 20 == 0
 
-# Started from 40 'cos it's the first number 20 can divide.
+
 try_num = 40
 switch = False
-
-# if str(try_num)[-1] != 0:
-#     try_num *= 10
 
 while switch is not True:
     if checknum(try_num):
         print("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is", try_num)
         switch = True
     else:
-        try_num += 10
+        try_num += 20
